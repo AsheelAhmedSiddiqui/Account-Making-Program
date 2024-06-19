fe3a44e2e5a3e05bc3b6eead145e003d9a26ed64
AsheelAhmedSiddiqui <asheelahmedsiddiqui@gmail.com>
2024-06-20 00:39:35 +0500

    updated

--- main.js
+++ main.js
@@ -6,24 +6,35 @@ let newPassword = document.getElementById("newpassword");
 let counter = 0;
 
 function newAcc() {
-  localStorage.setItem(`user`, newUser.value);
-  localStorage.setItem(`email`, newEmail.value);
-  localStorage.setItem(`password`, newPassword.value);
-  alert("Account created successfully");
-  window.location.href = "index.html";
+	localStorage.setItem(`user`, newUser.value);
+	localStorage.setItem(`email`, newEmail.value);
+	localStorage.setItem(`password`, newPassword.value);
+	alert("Account created successfully");
+	window.open = "index.html";
 }
-
 // for exisit users
 let loginUser = document.getElementById("username");
 let loginPass = document.getElementById("password");
 let loginBtn = document.getElementById("login");
 
 loginBtn.addEventListener("click", () => {
-  let getUser = localStorage.getItem("user");
-  let getPassword = localStorage.getItem("password");
-  if (loginUser.value == getUser && loginPass == getPassword) {
-    window.open = "dashboard.html";
-  } else {
-    alert("wrong");
-  }
+	// let getUser = localStorage.getItem("user");
+	// let getPassword = localStorage.getItem("password");
+	// if (loginUser.value == getUser && loginPass == getPassword) {
+	//   window.open = "dashboard.html";
+	// } else {
+	//   alert("wrong");
+	// }
+
+	if (
+		loginUser.value == localStorage.getItem("user") &&
+		loginPass.value == localStorage.getItem("password")
+	) {
+		console.log("asheel");
+		window.location.href = "dashboard.html";
+	} else {
+		console.log("no");
+	}
+	// console.log(getUser);
+	// console.log(getPassword);
 });
